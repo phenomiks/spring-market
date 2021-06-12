ac4dce8d063b01261a38ccf9c559c329e2c5a7a6
phenomiks <phenomikspc@gmail.com>
2021-06-12 15:22:20 +0300

    added new software to init.sql

--- src/main/resources/db/migration/V2__init.sql
+++ src/main/resources/db/migration/V2__init.sql
@@ -22,7 +22,24 @@ VALUES (1, 1),
 INSERT INTO products (title, price)
 VALUES ('Adobe Photoshop Elements 2021', 150),
        ('CLIP STUDIO PAINT PRO', 60),
-       ('CorelDRAW Graphics Suite 2020', 340);
+       ('CorelDRAW Graphics Suite 2020', 340),
+       ('Corel PaintShop Pro 2021 Ultimate', 70),
+       ('Cyberlink PowerDirector 19 Ultimate', 140),
+       ('Corel VideoStudio Ultimate 2020', 50),
+       ('Webroot Antivirus Software 2021', 20),
+       ('Trend Micro Maximum Security 2021', 37),
+       ('McAfee LiveSafe Ultimate Protection for Unlimited Devices', 38),
+       ('Malwarebytes Premium 4.0 Latest Version', 100),
+       ('Norton Small Business - 5 Device', 100),
+       ('AVG Technologies AVG Internet Security 2020', 90),
+       ('Windows Server 2019 Standard', 250),
+       ('Windows Server 2016 Standard', 200),
+       ('Window 10 Home', 105),
+       ('OfficeSuite Home & Business 2021', 100),
+       ('Corel WordPerfect Office 2020 Home & Student', 60),
+       ('Window 7 Professional', 70),
+       ('Microsoft Project Professional 2019', 460),
+       ('Nuance Dragon NaturallySpeaking Premium 13', 204);
 
 INSERT INTO categories (title)
 VALUES ('Software'),
